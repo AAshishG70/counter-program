c59b14427cd43e2c1eb10b4d654a4024f84c42e3
Aashish Gubhaju <aashishgubhaju12@gmail.com>
2022-12-11 20:33:04 -0700

    Use useReducer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 
-const Counter = ({ initialValue = 0, one = 1 }) => {
-  const [count, setCount] = useState(initialValue);
+const Counter = ({ initialValue = { count: 0, total: 100 }, step = 1 }) => {
+  const [state, dispatch] = useReducer(countReducer, initialValue);
 
-  const increment = () => setCount(count + one);
-  return <button onClick={increment}>{count}</button>;
+  function countReducer(state, action) {
+    switch (action.type) {
+      case "increment":
+        return { ...state, count: state.count + 1 };
+        break;
+      case "decrement":
+        return { ...state, count: state.count - action.step };
+        break;
+      default:
+        break;
+    }
+  }
+
+  const increment = () => dispatch({ type: "increment" });
+  const decrement = () => dispatch({ type: "decrement", step });
+  return (
+    <>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <h1>{state.count}</h1>
+    </>
+  );
 };
 
 const App = () => {
