d5f78352da0d95e0b6a661ce07385f1bcc547ec9
Aashish Gubhaju <aashishgubhaju12@gmail.com>
2022-12-11 18:50:00 -0700

    State Management

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+
+const App = () => {
+  const [state, setState] = useState({ name: "Alex", age: "" });
+  console.log(state);
+
+  useEffect(() => {
+    setState((prevState) => ({ ...prevState, age: 25 }));
+  }, []);
+  // [] is the dependency array
+
+  return <div> Hello World! </div>;
+};
+
+export default App;
